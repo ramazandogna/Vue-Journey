7c9bc5dae167c2ebbc1d97a8fa6d525e63567aac
Ramazan Doğan <doganrmzn40@gmail.com>
2023-07-19 19:04:42 +0300

    First instalization for firebase project

---
+++ 10-Firebase-app/firebase-app/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="home">
+      <img
+         alt="Vue logo"
+         src="../assets/logo.png"
+      />
+      <div>Home</div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'HomeView',
+   components: {},
+   setup() {
+      console.log('Setup yüklendi.');
+   },
+};
+</script>
