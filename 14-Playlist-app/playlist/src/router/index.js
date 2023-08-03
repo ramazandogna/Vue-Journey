b0830b336e338f56114bc0f356178ff3bc4e5327
Ramazan Doğan <doganrmzn40@gmail.com>
2023-08-04 00:04:37 +0300

    Rootes

--- 14-Playlist-app/playlist/src/router/index.js
+++ 14-Playlist-app/playlist/src/router/index.js
@@ -1,11 +1,26 @@
+//router imports
 import { createRouter, createWebHistory } from 'vue-router';
+//views import
 import Home from '../views/Home.vue';
+//firebase imports
+
+import { projectAuth } from '@/firebase/config';
+
+const requireAuth = (to, from, next) => {
+   const user = projectAuth.currentUser;
+   if (!user) {
+      next({ name: 'login' });
+   } else {
+      next();
+   }
+};
 
 const routes = [
    {
       path: '/',
       name: 'home',
       component: Home,
+      beforeEnter: requireAuth,
    },
    {
       path: '/login',
@@ -21,6 +36,14 @@ const routes = [
       path: '/playlist/create',
       name: 'createplaylist',
       component: () => import('../views/playlist/CreatePlaylist.vue'),
+      beforeEnter: requireAuth,
+   },
+   {
+      path: '/playlist:id',
+      name: 'playlistdetails',
+      component: () => import('../views/playlist/PlaylistDetail.vue'),
+      beforeEnter: requireAuth,
+      props: true,
    },
 ];
 
