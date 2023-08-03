72600b42a696783d592b371875cbb61c7f55b042
Ramazan Doğan <doganrmzn40@gmail.com>
2023-08-03 22:57:30 +0300

    ListCard

--- 14-Playlist-app/playlist/src/composables/getCollection.js
+++ 14-Playlist-app/playlist/src/composables/getCollection.js
@@ -1,30 +1,31 @@
-import { ref } from "vue";
-import { projectFirestore } from "../firebase/config";
+import { ref } from 'vue';
+import { projectFirestore } from '../firebase/config';
 
 const getCollection = (collection) => {
-  const documents = ref(null);
-  const error = ref(null);
+   const documents = ref(null);
+   const error = ref(null);
+   const isPending = ref(false);
 
-  let collectionRef = projectFirestore
-    .collection(collection)
-    .orderBy("createdAt");
+   let collectionRef = projectFirestore.collection(collection).orderBy('createdAt');
 
-  collectionRef.onSnapshot(
-    (snap) => {
-      let result = [];
-      snap.docs.forEach((doc) => {
-        doc.data().createdAt && result.push({ ...doc.data(), id: doc.id });
-      });
-      documents.value = result;
-      error.value = null;
-    },
-    (err) => {
-      console.log(err.message);
-      documents.value = null;
-      error.value = "could not fetch data";
-    }
-  );
-  return { documents, error };
+   collectionRef.onSnapshot(
+      (snap) => {
+         isPending.value = true;
+         let result = [];
+         snap.docs.forEach((doc) => {
+            doc.data().createdAt && result.push({ ...doc.data(), id: doc.id });
+         });
+         documents.value = result;
+         error.value = null;
+      },
+      (err) => {
+         console.log(err.message);
+         documents.value = null;
+         error.value = 'could not fetch data';
+         isPending.value = false;
+      }
+   );
+   return { documents, error, isPending };
 };
 
 export default getCollection;
--- 14-Playlist-app/playlist/src/views/Home.vue
+++ 14-Playlist-app/playlist/src/views/Home.vue
@@ -1,9 +1,33 @@
 <template>
-   <div class="home">Home</div>
+   <div
+      class="error"
+      v-if="error"
+   >
+      Couldnt fetch data..
+   </div>
+   <div v-if="isPending">
+      <div v-if="documents">
+         <ListCard :playlists="documents" />
+      </div>
+      <div v-else>Yok</div>
+   </div>
+   <div v-else>Yükleniyor..</div>
 </template>
 
 <script>
+//composables import
+import getCollection from '@/composables/getCollection';
+//components
+import ListCard from '@/components/ListCard.vue';
 export default {
-   components: {},
+   components: { ListCard },
+   setup() {
+      const { error, documents, isPending } = getCollection('playlists');
+      console.log(documents.value);
+      console.log(documents);
+
+      return { error, documents, isPending };
+   },
+   components: { ListCard },
 };
 </script>
